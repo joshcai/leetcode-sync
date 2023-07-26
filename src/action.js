7355421e4d963e1f5fbf8457f599108d96e5368b
Mayuresh Dharwadkar <98738585+Mayureshd-18@users.noreply.github.com>
2023-07-26 22:40:07 +0530

    Update action.js

--- src/action.js
+++ src/action.js
@@ -23,7 +23,7 @@ const LANG_TO_EXTENSION = {
   'scala': 'scala',
   'swift': 'swift',
   'typescript': 'ts',
-  'elixir': 'exs',
+  'elixir': 'ex',
 };
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
